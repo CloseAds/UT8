a93172cf678d9192cd5c8167ebf199d6aa3bb8ff
Chema <c7336188@gmail.com>
2024-03-20 09:44:24 +0100

    ut8

---
+++ src/ut8_01_07.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ut8_01_07 {
+
+    public static void main(String[] args) {
+
+        JFrame ventana = new JFrame();
+
+        ventana.setLayout(new BorderLayout());
+        JPanel panelPageStart = new JPanel();
+
+        panelPageStart.add(new JButton("Botón 1"));
+        panelPageStart.add(new JButton("Botón 2"));
+        panelPageStart.add(new JButton("Botón 3"));
+        ventana.add(panelPageStart, BorderLayout.PAGE_START);
+
+        JPanel panelPageEnd = new JPanel(new GridLayout(1, 2));
+        panelPageEnd.add(new JButton("Botón 4"));
+        panelPageEnd.add(new JButton("Botón 5"));
+        ventana.add(panelPageEnd, BorderLayout.PAGE_END);
+
+        ventana.add(new JButton("LINE_START"), BorderLayout.LINE_START);
+        ventana.add(new JButton("LINE_END"), BorderLayout.LINE_END);
+        ventana.add(new JButton("CENTER"), BorderLayout.CENTER);
+
+    }
+
+}
---
+++ src/ut8_01_08.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class ut8_01_08 {
+
+    public static void main(String[] args) {
+
+        JFrame calculadora = new JFrame();
+
+        JTextField texto = new JTextField("0", JTextField.NORTH_WEST);
+        JTextField texto1 = new JTextField("0", (int) JTextField.BOTTOM_ALIGNMENT);
+
+        texto1.setBounds();
+
+
+        calculadora.setVisible(true);
+
+    }
+
+}
